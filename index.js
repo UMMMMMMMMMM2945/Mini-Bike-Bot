f979742caa250136e059803e761d0251a589dad0
UMMMMMMMMMM2945 <80438462+UMMMMMMMMMM2945@users.noreply.github.com>
2022-05-05 14:07:06 -0400

    Updated Rich Presence and Status

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  client.user.setPresence({
+    status: 'idle'
+});
+client.user.setActivity("Engine Idle",{type: "LISTENING"})
 });
 
 client.login(process.env.TOKEN);
