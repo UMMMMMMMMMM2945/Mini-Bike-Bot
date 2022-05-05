b94e26d5d2aac5c79a0aaee34d1c9645dfee835f
mfeinstein1101 <matt.feinstein1101@gmail.com>
2022-05-05 13:00:29 -0400

    update

--- index.js
+++ index.js
@@ -6,12 +6,4 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-});
-
 client.login(process.env.TOKEN);
