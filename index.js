3af410a85cc8fe1ef483d964e8d315f0ded4bc3a
mfeinstein1101 <matt.feinstein1101@gmail.com>
2022-05-05 21:59:29 -0400

    basic database functionallity, needs more

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const sqlite = require('sqlite3').verbose();
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
+//init, presence
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
@@ -10,15 +11,26 @@ client.on('ready', () => {
     status: 'idle'
   });
   client.user.setActivity("Engine Idle",{type: "LISTENING"});
-
-  let db = new sqlite.Database('./data.db', sqlite.OPEN_READWRITE);
 });
 
+//message event
 client.on('messageCreate', (message)=> {
   let msg = message.content.toLowerCase();
+  let prefix = process.env.PREFIX;
   if(message.author.bot) return;
-  let db = new sqlite.Database('./data.db', sqlite.OPEN_READWRITE);
-  db.run('CREATE TABLE IF NOT EXISTS data(userid INTEGER NOT NULL, username TEXT NOT NULL, )')
+  let uid = message.author.id;
+  //init database, create user table
+  let db = new sqlite.Database('./userdata.db', sqlite.OPEN_READWRITE);
+  db.run('CREATE TABLE IF NOT EXISTS bike(userid INTEGER NOT NULL, bikename TEXT NOT NULL, bikelink TEXT NOT NULL)');
+  if(msg[0]!=prefix) return;
+
+  let com = msg.substring(0, msg.indexOf(" "));
+  let args = msg.substring(msg.indexOf(" ")+1).split(",");
+  if(com=='~register' && args.length==2) {
+    let insertdata = db.prepare('INSERT INTO bike VALUES(?, ?, ?)');
+    insertdata.run(uid, args[0], args[1]);
+  }
+
 });
 
 client.login(process.env.TOKEN);
